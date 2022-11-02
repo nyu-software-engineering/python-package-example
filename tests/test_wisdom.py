58dd5d775fc652a641af7e4ceab236437cbc7f52
bloombar <amos@cs.nyu.edu>
2022-11-02 11:22:37 -0400

    adding fixtures

--- tests/test_wisdom.py
+++ tests/test_wisdom.py
@@ -2,10 +2,31 @@ import pytest
 from examplepackagefb1258 import wisdom
 
 class Tests:
-  
-  def test_sanity_check(self):
+
+  #
+  # Fixtures - these are functions that can do any optional setup or teardown before or after a test function is run.
+  #
+
+  def example_fixture(self):
+    '''
+    An example of a pytest fixture - a function that can be used for setup and teardown before and after test functions are run.
+    '''
+
+    # place any setup you want to do before any test function that uses this fixture is run
+
+    yield # at th=e yield point, the test function will run and do its business
+
+    # place with any teardown you want to do after any test function that uses this fixture has completed
+
+
+  #
+  # Test functions
+  #
+
+  def test_sanity_check(self, example_fixture):
     """
     Test debugging... making sure that we can run a simple test that always passes.
+    Note the use of the example_fixture in the parameter list - any setup and teardown in that fixture will be run before and after this test function executes
     From the main project directory, run the `python3 -m pytest` command to run all tests.
     """
     expected = True # the value we expect to be present
@@ -15,6 +36,7 @@ class Tests:
   def test_get(self):
     """
     Verify get() function and make sure it returns a non-empty string.
+    Note that for example purposes, we have not used the example_fixture in this test functino.
     """
     # since get returns a random string, run it a bunch of times and verify the output
     for i in range(100):
